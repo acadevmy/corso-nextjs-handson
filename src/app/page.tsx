13d4e2e8923462c067a43c1b214b48e8e51285d0
erikamolino <erikamolino15@gmail.com>
2024-04-24 16:50:05 +0200

    feat: custom global configurations and homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,154 @@
-import Image from "next/image";
+import clsx from "clsx";
+import Image, { ImageProps } from "next/image";
+
+import githubLogo from "@/images/github.png";
+import gridImage1 from "@/images/grid-image-1.jpeg";
+import gridImage2 from "@/images/grid-image-2.jpeg";
+import gridImage3 from "@/images/grid-image-3.jpeg";
+import gridImage4 from "@/images/grid-image-4.jpeg";
+import gridImage5 from "@/images/grid-image-5.jpeg";
+import gridImage6 from "@/images/grid-image-6.jpeg";
+import nextLogo from "@/images/next.svg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
+    <main className="max-w-2xl m-auto p-10">
+      <div className="mb-10">
+        <h1 className="mb-4">Ciao, Bevenuto nel mio Porfolio 👋</h1>
+        <h2 className="mb-10">
+          Proident laborum nulla dolore est laboris tempor enim non irure qui.
+        </h2>
+
+        <p>
+          Sint nisi dolor commodo esse id officia aute magna ad velit elit magna
+          quis.
+          <Badge
+            src={nextLogo}
+            alt="next.js logo"
+            href="https://nextjs.org"
+            label="Nextj.js"
+          />{" "}
+          Reprehenderit sunt deserunt cupidatat velit veniam. Veniam laborum
+          dolor incididunt deserunt{" "}
+          <Badge
+            src={githubLogo}
+            alt="github logo"
+            href="https://github.com"
+            label="Github"
+          />{" "}
+          aliqua fugiat in adipisicing.
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
       </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <div className="mb-10">
+        <ImageGrid />
+      </div>
+
+      <div className="mb-10">
+        <p>
+          Sint laborum aute occaecat adipisicing eiusmod sint aliqua laborum.
+          Mollit commodo velit officia velit excepteur pariatur commodo esse
+          commodo. Exercitation in duis non ipsum. Aliqua incididunt labore esse
+          elit aliquip reprehenderit officia esse anim consequat officia.
+        </p>
+      </div>
+
+      <div className="mb-10 flex gap-4">
+        <ChannelLink
+          src={githubLogo}
+          alt="github logo"
+          description="Seguimi su Github"
+          href="https://github.com"
+          title="@myusername"
+        />
+        <ChannelLink
+          src={githubLogo}
+          alt="github logo"
+          description="Seguimi su Github"
+          href="https://github.com"
+          title="@myusername"
         />
       </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
+  );
+}
+
+type BadgeProps = {
+  src: ImageProps["src"];
+  label: string;
+  alt: string;
+  href: string;
+};
+
+function Badge({ src, alt, label, href }: BadgeProps) {
+  return (
+    <a
+      href={href}
+      className="bg-zinc-700 border border-zinc-500 p-0.5 text-xs inline-flex items-center gap-1"
+    >
+      <Image src={src} alt={alt} className="w-4 h-4 object-contain" />
+      {label}
+    </a>
+  );
+}
+
+function ImageGrid() {
+  const imageClassName = "rounded-md object-cover h-80 mb-6";
+  return (
+    <div className="sm:columns-3 gap-2">
+      <Image
+        alt=""
+        src={gridImage1}
+        className={clsx(imageClassName, "sm:h-40 sm:mb-2")}
+      />
+      <Image
+        alt=""
+        src={gridImage2}
+        className={clsx(imageClassName, "sm:h-60")}
+      />
+      <Image
+        alt=""
+        src={gridImage3}
+        className={clsx(imageClassName, "sm:h-60 sm:mb-2")}
+      />
+      <Image
+        alt=""
+        src={gridImage4}
+        className={clsx(imageClassName, "sm:h-40")}
+      />
+      <Image
+        alt=""
+        src={gridImage5}
+        className={clsx(imageClassName, "sm:h-40 sm:mb-2")}
+      />
+      <Image
+        alt=""
+        src={gridImage6}
+        className={clsx(imageClassName, "sm:h-60")}
+      />
+    </div>
+  );
+}
+
+type ChannelLinkProps = {
+  title: string;
+  description: string;
+  href: string;
+  src: ImageProps["src"];
+  alt: ImageProps["alt"];
+};
+function ChannelLink({ title, description, href, src, alt }: ChannelLinkProps) {
+  return (
+    <a
+      href={href}
+      className="flex-1 flex items-center rounded-t-md gap-3 p-2 border border-zinc-500 bg-zinc-700"
+    >
+      <Image src={src} alt={alt} className="w-10 h-10 object-contain" />
+
+      <div>
+        <p className="text-sm font-bold">{title}</p>
+        <p className="text-xs mt-2">{description}</p>
+      </div>
+    </a>
   );
 }

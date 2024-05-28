38870495219f3996137c651de0c137a9e8e5507a
erikamolino <erikamolino15@gmail.com>
2024-05-28 14:29:48 +0200

    fix: fixed useSearchParams Suspense in auth/error

--- src/app/auth/error/page.tsx
+++ src/app/auth/error/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useParams, useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 
 enum AuthError {
   Configuration = "Configuration",
@@ -18,15 +19,21 @@ const errorMap = {
 };
 
 export default function AuthErrorPage() {
-  const search = useSearchParams();
-  const authError = search.get("error") as AuthError;
-
   return (
     <div className="flex border border-red-300 bg-red-500/50 p-8 rounded-md">
       <div>
         <p className="text-xl mb-4">Errore nell&apos;autenticazione</p>
-        <p>{errorMap[authError]}</p>
+        <Suspense>
+          <ErrorMapMessage />
+        </Suspense>
       </div>
     </div>
   );
+}
+
+function ErrorMapMessage() {
+  const search = useSearchParams();
+  const authError = search.get("error") as AuthError;
+
+  return <p>{errorMap[authError]}</p>;
 }
